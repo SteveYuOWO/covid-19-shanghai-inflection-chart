8396df9eeb6b5e77a7abd54eba0d96e5a87f43a4
Steve Yu <steveyuowo@gmail.com>
2022-04-09 16:50:39 +0800

    Update legend and title

--- readfromcsv.py
+++ readfromcsv.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import matplotlib 
-import numpy as np
 
-matplotlib.rc("font", family="Heiti TC")
+plt.rc("font", family="Heiti TC")
+plt.rcParams["figure.autolayout"] = True
 
 fig, ax = plt.subplots()
 fig.autofmt_xdate()
+fig.canvas.set_window_title('上海感染者折线图')
 
 df = pd.read_csv('./data.csv')
 
@@ -14,10 +14,11 @@ date = df.loc[:, "日期"].values
 inflection_people = df.loc[:, "感染者"].values[::-1]
 asymptomatic_people = df.loc[:, "无症状感染者"].values[::-1]
 
+plt.title("上海感染者折线图")
+plt.xlabel("日期")
+plt.ylabel("人数")
 plt.plot(date, inflection_people, label="感染者")
 plt.plot(date, asymptomatic_people, label="无症状感染者")
 
+plt.legend(loc="upper right")
 plt.show()
-# print(date)
-# print(inflection_people)
-# print(asymptomatic_people)
