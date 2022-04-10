2e7feea56f563fda4e2966cdd63a52688227edbc
Steve Yu <steveyuowo@gmail.com>
2022-04-10 14:02:27 +0800

    Update lengend loc to default

--- exportpng.py
+++ exportpng.py
@@ -13,9 +13,8 @@ df = pd.read_csv('./data.csv')
 date = df.loc[:, "日期"].values[::-1]
 inflection_people = df.loc[:, "感染者"].values[::-1]
 asymptomatic_people = df.loc[:, "无症状感染者"].values[::-1]
-print(len(inflection_people))
 
-plt.figure(figsize=(len(inflection_people), 6), dpi=300)
+plt.figure(figsize=(len(inflection_people) * 1.2, 6), dpi=300)
 
 plt.title("上海感染者折线图")
 plt.xlabel("日期")
@@ -23,5 +22,5 @@ plt.ylabel("人数")
 plt.plot(date, inflection_people, label="感染者")
 plt.plot(date, asymptomatic_people, label="无症状感染者")
 
-plt.legend(loc="upper left")
+plt.legend()
 plt.savefig('preview.png')
