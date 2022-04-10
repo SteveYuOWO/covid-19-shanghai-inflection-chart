b4d6a9e8128b4183833953eb0247d2dde76dd259
Steve Yu <steveyuowo@gmail.com>
2022-04-10 14:43:36 +0800

    map date to zh

--- exportpng.py
+++ exportpng.py
@@ -1,17 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 fig, ax = plt.subplots()
 fig.autofmt_xdate()
-fig.canvas.set_window_title('Shanghai Infected Persons Line Chart')
+fig.canvas.manager.set_window_title('Shanghai Infected Persons Line Chart')
 
 df = pd.read_csv('./data.csv')
 
-date = df.loc[:, "日期"].values[::-1]
+date = np.array(list(map(lambda x: x.replace("月", ".").replace("日", ""), df.loc[:, "日期"].values[::-1])))
 inflection_Persons = df.loc[:, "感染者"].values[::-1]
 asymptomatic_Persons = df.loc[:, "无症状感染者"].values[::-1]
 
-plt.figure(figsize=(len(inflection_Persons) * 1.2, 6), dpi=300)
+plt.figure(figsize=(len(inflection_Persons) * 0.6, 6), dpi=300)
 
 plt.title("Shanghai Infected Persons Line Chart")
 plt.xlabel("Date")
